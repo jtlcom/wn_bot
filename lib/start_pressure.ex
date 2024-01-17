391bf20c12e875b0968ff24f3d8752775758da9a
Jia Tian-Lin <jtlcom0316@gmail.com>
2024-01-17 18:02:39 +0800

    优化

--- lib/start_pressure.ex
+++ lib/start_pressure.ex
@@ -1,44 +1,7 @@
 defmodule StartPressure do
   require Logger
-
-  def go(server_ip, server_port, name_prefix, from, to, born_state, ai) do
-    # StartPressure.go('192.168.1.129', 6666, "bot_1_", 1, 2, 1)
-    start_some(server_ip, server_port, name_prefix, from, to, born_state, ai)
-  end
-
-  def start_single(server_ip, server_port, name_prefix, id, born_state, ai) do
-    account = name_prefix <> "#{id}"
-
-    case HTTPoison.post(
-           "http://192.168.1.92:8080/auth/whynot",
-           %{"account" => account, "password" => "111111"} |> URI.encode_query(),
-           [{"Content-Type", "application/x-www-form-urlencoded"}],
-           timeout: 5000
-         ) do
-      {:ok, %{body: body}} ->
-        case Jason.decode!(body) do
-          %{"login_with_data" => login_with_data, "token" => token} ->
-            case Avatar.Supervisor.start_child(
-                   {server_ip, server_port, account, born_state, ai, token, Jason.encode!(login_with_data)},
-                   name: {:global, {:name, Guid.name(id)}}
-                 ) do
-              {:ok, pid} ->
-                pid
-
-              _ ->
-                nil
-            end
-
-          _ ->
-            :error
-        end
-
-      {:error, _error} ->
-        :error
-    end
-  end
-
-  def start_some(server_ip, server_port, name_prefix, from_id, to_id, born_state, ai) do
+  # StartPressure.go('192.168.1.129', 6666, "bot_1_", 1, 2, 1)
+  def go(server_ip, server_port, name_prefix, from_id, to_id, born_state, ai) do
     try_one =
       :gen_tcp.connect(server_ip, server_port, [
         :binary,
@@ -55,7 +18,7 @@ defmodule StartPressure do
         strategy(:once_time, server_ip, server_port, name_prefix, from_id, to_id, born_state, ai)
 
       _ ->
-        Logger.warn("cannot connect #{inspect(server_ip)}:#{inspect(server_port)}")
+        Logger.warning("cannot connect #{inspect(server_ip)}:#{inspect(server_port)}")
     end
   end
 
@@ -65,5 +28,38 @@ defmodule StartPressure do
       # Process.sleep(300)
       start_single(server_ip, server_port, name_prefix, this_id, born_state, ai)
     end)
+  end
+
+  def start_single(server_ip, server_port, name_prefix, id, born_state, ai) do
+    account = name_prefix <> "#{id}"
+
+    case HTTPoison.post(
+           "http://192.168.1.92:8080/auth/whynot",
+           %{"account" => account, "password" => "111111"} |> URI.encode_query(),
+           [{"Content-Type", "application/x-www-form-urlencoded"}],
+           timeout: 5000
+         ) do
+      {:ok, %{body: body}} ->
+        case Jason.decode!(body) do
+          %{"login_with_data" => login_with_data, "token" => token} ->
+            case Avatar.Supervisor.start_child(
+                   {server_ip, server_port, account, born_state, ai, token,
+                    Jason.encode!(login_with_data)},
+                   name: {:global, {:name, Guid.name(id)}}
+                 ) do
+              {:ok, pid} ->
+                pid
+
+              _ ->
+                nil
+            end
+
+          _ ->
+            :error
+        end
+
+      {:error, _error} ->
+        :error
+    end
   end
 end
